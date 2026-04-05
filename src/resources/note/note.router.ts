b97b70f6d478713ee8e3be637367d1b80e013b71
renatosysadmin <88123588+renatosysadmin@users.noreply.github.com>
2026-04-04 20:42:26 -0400

    Update note.router.ts
    Update

--- src/resources/note/note.router.ts
+++ src/resources/note/note.router.ts
@@ -1,1 +1,27 @@
+import { Router } from 'express';
+import { noteController } from './note.controller';
+import { isAuth } from '../../middlewares/isAuth';
+import { validateBody } from '../../middlewares/validateBody';
+import { noteSchema } from './note.schema';
 
+const noteRouter = Router();
+
+// Todas as rotas de notas requerem sessão ativa
+noteRouter.use(isAuth);
+
+// GET /v1/notes - Listar todas
+noteRouter.get('/', noteController.index);
+
+// POST /v1/notes - Criar nova
+noteRouter.post('/', validateBody(noteSchema), noteController.create);
+
+// GET /v1/notes/:id - Ver específica
+noteRouter.get('/:id', noteController.show);
+
+// PUT /v1/notes/:id - Atualizar
+noteRouter.put('/:id', validateBody(noteSchema), noteController.update);
+
+// DELETE /v1/notes/:id - Remover
+noteRouter.delete('/:id', noteController.remove);
+
+export { noteRouter };
