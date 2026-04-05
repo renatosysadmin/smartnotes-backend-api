bb979562cae36eb5b8904120657cc2240aa4a686
renatosysadmin <88123588+renatosysadmin@users.noreply.github.com>
2026-04-04 20:35:57 -0400

    Update auth.schema.ts
    Update

--- src/resources/auth/auth.schema.ts
+++ src/resources/auth/auth.schema.ts
@@ -1,1 +1,15 @@
+import Joi from 'joi';
 
+export const signupSchema = Joi.object({
+  email: Joi.string().email().max(100).required(),
+  fullname: Joi.string().max(100).required(),
+  password: Joi.string()
+    .min(8).max(128)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+    .required()
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
