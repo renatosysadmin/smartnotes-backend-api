3f39bded29e33cef72d0c8451a4bdee6daf9124c
renatosysadmin <88123588+renatosysadmin@users.noreply.github.com>
2026-04-04 20:36:49 -0400

    Update isAuth.ts
    Update

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,1 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 
+export const isAuth = (req: Request, res: Response, next: NextFunction) => {
+  if (req.session && req.session.userId) {
+    return next();
+  }
+  return res.status(401).json({ msg: "Usuário não autenticado" });
+};
