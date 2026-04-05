7d0229a039d68d2978ac7f7b67bce61b68ce387d
renatosysadmin <88123588+renatosysadmin@users.noreply.github.com>
2026-04-04 20:36:30 -0400

    Update validateBody.ts
    Update

--- src/middlewares/validateBody.ts
+++ src/middlewares/validateBody.ts
@@ -1,1 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+import { Schema } from 'joi';
 
+export const validateBody = (schema: Schema) => (req: Request, res: Response, next: NextFunction) => {
+  const { error } = schema.validate(req.body, { abortEarly: false });
+  if (error) return res.status(422).json({ errors: error.details.map(d => d.message) });
+  next();
+};
