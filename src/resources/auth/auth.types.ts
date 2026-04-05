eca0ab14b06f540486869debff2ba70a800dc524
renatosysadmin <88123588+renatosysadmin@users.noreply.github.com>
2026-04-04 20:40:13 -0400

    Update auth.types.ts
    Update

--- src/resources/auth/auth.types.ts
+++ src/resources/auth/auth.types.ts
@@ -1,1 +1,11 @@
+import { User } from '@prisma/client';
 
+// Dados necessários para criar um novo usuário (Signup)
+// Usamos o Pick para garantir que venha exatamente o que o banco espera
+export type SignupData = Pick<User, 'email' | 'fullname' | 'password'>;
+
+// Dados necessários para o Login
+export type LoginData = Pick<User, 'email' | 'password'>;
+
+// Tipo para a resposta do usuário (omitindo a senha por segurança)
+export type UserResponse = Omit<User, 'password'>;
